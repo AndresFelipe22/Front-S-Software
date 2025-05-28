1db1d5a477b5b77bdf0b1abc1fb36457c36837ff
Yesid-26 <gameryemm@gmail.com>
2025-05-28 15:18:39 -0500

    Update BecomeSellerFormStep3.tsx

--- src/customer/pages/Become Seller/BecomeSellerFormStep3.tsx
+++ src/customer/pages/Become Seller/BecomeSellerFormStep3.tsx
@@ -1,54 +1,75 @@
-import React from 'react'
-import { Grid, TextField} from "@mul/material"
+import React from 'react';
+import { Box, TextField } from '@mui/material';
+import { FormikProps } from 'formik';
 
-const BecomeSellerFormStep2Props = {
-    formik:any; 
+interface FormValues {
+  bankDetails: {
+    accountNumber: string;
+    ifscCode: string;
+    accountHolderName: string;
+  };
 }
-const BecomeSellerFormStep3: React.FC<BecomeSellerFormStep2Props> = ({formik}) => {
 
-    return (
-        <Box>
-            <p className='text-xl font-bold text-center pb-9'>Contact Details</p>
-        <div className ='space-y-5'>
+interface BecomeSellerFormStep3Props {
+  formik: FormikProps<FormValues>;
+}
 
+const BecomeSellerFormStep3: React.FC<BecomeSellerFormStep3Props> = ({ formik }) => {
+  return (
+    <Box>
+      <p className='text-xl font-bold text-center pb-9'>Bank Details</p>
+      <div className='space-y-5'>
         <TextField
-            fullWidth
-            name="bankDetails.accountNumber"
-            label="Account Number"
-            value={formik.values.bankDetails.accountNumber}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.bankDetails?.accountNumber && Boolean(formik.errors.
-            bankDetails?.accountNumber)}
-            helperText={formik.touched.bankDetails?.accountNumber && formik.errors.
-            bankDetails?.accountNumber}
-            />
+          fullWidth
+          name="bankDetails.accountNumber"
+          label="Account Number"
+          value={formik.values.bankDetails.accountNumber}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.bankDetails?.accountNumber &&
+            Boolean(formik.errors.bankDetails?.accountNumber)
+          }
+          helperText={
+            formik.touched.bankDetails?.accountNumber &&
+            formik.errors.bankDetails?.accountNumber
+          }
+        />
         <TextField
-            fullWidth
-            name="bankDetails.ifscCode"
-            label="IFSC Code"
-            value={formik.values.bankDetails.ifscCode}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.bankDetails?.ifscCode && Boolean(formik.errors.
-            bankDetails?.ifscCode)}
-            helperText={formik.touched.bankDetails?.ifscCode && formik.errors.
-            bankDetails?.ifscCode}
-            />
+          fullWidth
+          name="bankDetails.ifscCode"
+          label="IFSC Code"
+          value={formik.values.bankDetails.ifscCode}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.bankDetails?.ifscCode &&
+            Boolean(formik.errors.bankDetails?.ifscCode)
+          }
+          helperText={
+            formik.touched.bankDetails?.ifscCode &&
+            formik.errors.bankDetails?.ifscCode
+          }
+        />
         <TextField
-            fullWidth
-            name="bankDetails.accountHolderName"
-            label="Account Holder Name"
-            value={formik.values.bankDetails.accountHolderName}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.bankDetails?.accountHolderName && Boolean(formik.
-            errors.bankDetails?.accountHolderName)}
-            helperText={formik.touched.bankDetails?.ifscCode && formik.
-            errors.bankDetails?.accountHolderName}
-            />
-        </div>
-        </Box>
-    );
+          fullWidth
+          name="bankDetails.accountHolderName"
+          label="Account Holder Name"
+          value={formik.values.bankDetails.accountHolderName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={
+            formik.touched.bankDetails?.accountHolderName &&
+            Boolean(formik.errors.bankDetails?.accountHolderName)
+          }
+          helperText={
+            formik.touched.bankDetails?.accountHolderName &&
+            formik.errors.bankDetails?.accountHolderName
+          }
+        />
+      </div>
+    </Box>
+  );
 };
-export default BecomeSellerFormStep3
+
+export default BecomeSellerFormStep3;
